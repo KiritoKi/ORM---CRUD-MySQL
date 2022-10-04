3e61f8590812e2b6f0b824b61027661df8137452
KiritoKi <yaneliteboy@hotmail.com>
2022-10-03 21:06:54 -0300

    added gitignore

--- controller/db.js
+++ controller/db.js
@@ -2,20 +2,11 @@ import { createConnection } from 'mysql';
 
 var connection = createConnection({
     host: 'localhost',
-<<<<<<< HEAD
-    user: '*****',
-    password: '******',
-=======
     user: 'kirito',
     password: 'yanVP&123456',
->>>>>>> master
     database: 'db_faculdade'
 });
 
 connection.connect();
 
-<<<<<<< HEAD
 export default connection;
-=======
-export default connection;
->>>>>>> master
